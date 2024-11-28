0e359403945dc065ee70339a6f45908e89150408
Igor Gissi <gissiigor@gmail.com>
2024-11-28 10:27:21 -0300

    estruturação do projeto

---
+++ index.js
@@ -0,0 +1,33 @@
+let nome = "Kyupes"
+let xp = 8654
+let nivel
+
+switch(xp){
+    case xp < 1000:
+        nivel = "Ferro"
+        break
+    case 1001 < xp < 2000:
+        nivel = "Bronze"
+        break
+    case 2001 < xp < 5000:
+        nivel = "Prata"
+        break
+    case 5001 < xp < 7000:
+        nivel = "Ouro"
+        break
+    case 7001 < xp < 8000:
+        nivel = "Platina"
+        break
+    case 8001 < xp < 9000:
+        nivel = "Ascendente"
+        break
+    case 9001 < xp < 10000:
+        nivel = "Imortal"
+        break
+    case xp >= 10001:
+        nivel = "Radiante"
+        break
+    default:
+        nivel = "nível não reconhecido"
+}
+console.log("O Herói de nome " + nome + " está no nível de " + nivel)
