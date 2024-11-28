98c0ed5138f49b455777f629a41ae111c53c7393
Igor Gissi <gissiigor@gmail.com>
2024-11-28 10:52:10 -0300

    finalização do projeto

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
-let nome = "Kyupes"
-let xp = 8654
-let nivel
+let nome = "Kyupes";
+let xp = 8654;
+let nivel = "";
 
-switch(xp){
-    case xp < 1000:
-        nivel = "Ferro"
-        break
-    case 1001 < xp < 2000:
-        nivel = "Bronze"
-        break
-    case 2001 < xp < 5000:
-        nivel = "Prata"
-        break
-    case 5001 < xp < 7000:
-        nivel = "Ouro"
-        break
-    case 7001 < xp < 8000:
-        nivel = "Platina"
-        break
-    case 8001 < xp < 9000:
-        nivel = "Ascendente"
-        break
-    case 9001 < xp < 10000:
-        nivel = "Imortal"
-        break
-    case xp >= 10001:
-        nivel = "Radiante"
-        break
-    default:
-        nivel = "nível não reconhecido"
+if (xp < 1000){
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000){
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000){
+    nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000){
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000){
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000){
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000){
+    nivel = "Imortal";
+} else if (xp >= 10001){
+    nivel = "Radiante";
 }
-console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+console.log("O Herói de nome " + nome + " está no nível de " + nivel);
